2ecbb1e807fec3219a9dd227c4626f42b1037c0c
ywisnu <starbox8888@gmail.com>
2024-07-09 17:43:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ pg = st.navigation(
 
 # --- SHARED ON ALL PAGES ---
 st.logo("assets/codingisfun_logo.png")
-st.sidebar.text("Made with ❤️ by Sven")
+st.sidebar.text("Every Accomplishment Starts With The Decision To Try")
 
 
 # --- RUN NAVIGATION ---
